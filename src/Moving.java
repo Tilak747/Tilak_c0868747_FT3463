877a4b8dea4160c6bc46e716fc5c39292384196f
Tilak <c0868747@mylambton.ca>
2022-11-21 21:02:45 -0500

    -create Moving class and add functions.

---
+++ src/Moving.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Moving {
+
+    private final List<Box> boxes;
+
+    public Moving(int size){
+        boxes = new ArrayList<>(size);
+    }
+
+    public void addBox(Box box){
+        boxes.add(box);
+    }
+
+    public String find(String name){
+        return "";
+    }
+
+    public void print(){
+
+    }
+
+
+
+}
